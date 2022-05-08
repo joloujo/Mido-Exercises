a3a5bba043f6933c67a556d1e09054380075d7ca
joloujo <joloujo28@gmail.com>
2022-05-08 12:56:07 -0700

    Exercise 2
    Completed exercise 2

---
+++ Mido-Exercises/exercise2.py
@@ -0,0 +1,18 @@
+import mido
+
+# load midi file
+midi_file = mido.MidiFile("../data/liebesleid.mid")
+
+# print midi file (used for debugging)
+# for i, track in enumerate(midi_file.tracks):
+#     print('Track {}: {}'.format(i, track.name))
+#     for j, event in enumerate(track):
+#         print('{}: {}'.format(j, event))
+
+for i, track in enumerate(midi_file.tracks):  # for each track...
+    for j, event in enumerate(track):  # for each event in the track
+        if event.type == "note_on" or event.type == "note_off":  # if that event turns a not on or turns a note off
+            event.note += 2  # transpose its note up a major 2nd
+
+# save the altered midi file
+midi_file.save("../data/exercise2_output.mid")
